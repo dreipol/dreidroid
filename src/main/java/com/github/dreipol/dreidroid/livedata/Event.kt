0c5ff3b146f3dde1ebc72fe0f10d92869eea86ff
Kai Widmer <kaiwi@bluewin.ch>
2020-03-26 10:31:12 +0100

    Add Event class for LiveData

---
+++ src/main/java/com/github/dreipol/dreidroid/livedata/Event.kt
@@ -0,0 +1,28 @@
+package com.github.dreipol.dreidroid.livedata
+
+/**
+ * A class witch allows in combination with LiveData the observer to check if the new value was already handled
+ * also see: https://android.jlelse.eu/sending-events-from-viewmodel-to-activities-fragments-the-right-way-26bb68502b24
+ */
+open class Event<out T>(private val content: T) {
+
+    var hasBeenHandled = false
+        private set // Allow external read but not write
+
+    /**
+     * Returns the content and prevents its use again.
+     */
+    fun getContentIfNotHandledOrReturnNull(): T? {
+        return if (hasBeenHandled) {
+            null
+        } else {
+            hasBeenHandled = true
+            content
+        }
+    }
+
+    /**
+     * Returns the content, even if it's already been handled.
+     */
+    fun peekContent(): T = content
+}

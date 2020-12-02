d9612ef7299548b38d10a7bfcad273afc0dbf284
Kai Widmer <>
2020-12-02 09:52:40 +0100

    Make overridden animation nullable

--- src/main/java/com/github/dreipol/dreidroid/utils/AnimationHelper.kt
+++ src/main/java/com/github/dreipol/dreidroid/utils/AnimationHelper.kt
@@ -6,22 +6,35 @@ object AnimationHelper {
 
     private var enterAnimation: Animation? = null
     private var exitAnimation: Animation? = null
+    private var overrideNextEnterAnimation = false
+    private var overrideNextExitAnimation = false
 
-    fun overrideNextFragmentEnterAnimation(animation: Animation) {
+    fun overrideNextFragmentEnterAnimation(animation: Animation?) {
+        overrideNextEnterAnimation = true
         enterAnimation = animation
     }
 
-    fun overrideNextFragmentExitAnimation(animation: Animation) {
+    fun overrideNextFragmentExitAnimation(animation: Animation?) {
+        overrideNextExitAnimation = true
         exitAnimation = animation
     }
 
+    fun shouldOverrideAnimation(enter: Boolean): Boolean {
+        if (enter) {
+            return overrideNextEnterAnimation
+        }
+        return overrideNextExitAnimation
+    }
+
     fun getNextFragmentEnterAnimation(): Animation? {
+        overrideNextEnterAnimation = false
         val currentAnimation = enterAnimation
         enterAnimation = null
         return currentAnimation
     }
 
     fun getNextFragmentExitAnimation(): Animation? {
+        overrideNextExitAnimation = false
         val currentAnimation = exitAnimation
         exitAnimation = null
         return currentAnimation

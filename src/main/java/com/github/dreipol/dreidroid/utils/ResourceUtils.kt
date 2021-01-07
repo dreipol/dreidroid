b09370bdc9a772f39275515f00d3839cd1758000
Kai Widmer <>
2021-01-07 13:55:38 +0100

    [gh-pages] clean branche

--- src/main/java/com/github/dreipol/dreidroid/utils/ResourceUtils.kt
+++
@@ -1,8 +0,0 @@
-package com.github.dreipol.dreidroid.utils
-
-import android.content.Context
-
-fun dp2px(dp: Int, context: Context): Int {
-    val scale = context.resources.displayMetrics.density
-    return (dp * scale + 0.5).toInt()
-}

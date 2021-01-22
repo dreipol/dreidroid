48e73bd50ee6666c0b8ff803edb879758b9f3480
Kai Widmer <>
2021-01-22 15:08:16 +0100

    Add TouchSizeIncreaser

---
+++ src/main/java/com/github/dreipol/dreidroid/components/TouchSizeIncreaser.kt
@@ -0,0 +1,33 @@
+package com.github.dreipol.dreidroid.components
+
+import android.content.Context
+import android.util.AttributeSet
+import android.view.MotionEvent
+import android.widget.FrameLayout
+
+/**
+ * Can be used to increase the touch-size of its wrapped child
+ *
+ * Example:
+ * '''
+ * <com.github.dreipol.dreidroid.components.TouchSizeIncreaser
+    android:layout_width="wrap_content"
+    android:layout_height="wrap_content"
+    android:padding="8dp">
+        <Spinner
+            android:layout_width="wrap_content"
+            android:layout_height="wrap_content"
+            android:layout_gravity="center"/>
+    </ch.tutti.ui.util.TouchSizeIncreaser>
+ * '''
+ */
+class TouchSizeIncreaser(context: Context, attrs: AttributeSet?) : FrameLayout(context, attrs) {
+    override fun onInterceptTouchEvent(event: MotionEvent): Boolean {
+        return true
+    }
+
+    override fun onTouchEvent(event: MotionEvent): Boolean {
+        getChildAt(0)?.onTouchEvent(event)
+        return true
+    }
+}

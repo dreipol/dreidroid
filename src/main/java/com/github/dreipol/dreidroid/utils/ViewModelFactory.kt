0af4bc7fcaf5848e2bc53ce5d031b990d5b04c2e
Samuel Bichsel <samuel.bichsel@dreipol.ch>
2024-11-22 15:08:08 +0100

    Fix another error

--- src/main/java/com/github/dreipol/dreidroid/utils/ViewModelFactory.kt
+++ src/main/java/com/github/dreipol/dreidroid/utils/ViewModelFactory.kt
@@ -4,9 +4,15 @@ import androidx.fragment.app.Fragment
 import androidx.fragment.app.FragmentActivity
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.ViewModelProvider
+import androidx.lifecycle.ViewModelProvider.Factory.Companion
+import androidx.lifecycle.viewmodel.CreationExtras
 
 public class BaseViewModelFactory<T>(internal val creator: () -> T) : ViewModelProvider.Factory {
-    override fun <T : ViewModel?> create(modelClass: Class<T>): T {
+    public override fun <T : ViewModel> create(modelClass: Class<T>): T {
+        return creator() as T
+    }
+
+    public override fun <T : ViewModel> create(modelClass: Class<T>, extras: CreationExtras): T {
         return creator() as T
     }
 }
